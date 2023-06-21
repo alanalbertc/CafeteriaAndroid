04dd0231331ce747f5c908fa3475553fdadebd34
alanalbertc <68207532+alanalbertc@users.noreply.github.com>
2023-06-20 21:11:32 -0600

    Se agregó linear layout y la vista de item

--- app/src/main/java/com/example/cafeteria/MainActivity.kt
+++ app/src/main/java/com/example/cafeteria/MainActivity.kt
@@ -2,10 +2,35 @@ package com.example.cafeteria
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.content.Intent
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.ListView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var listView: ListView
+    private lateinit var items: ArrayList<String>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        listView = findViewById(R.id.listView)
+        items = arrayListOf("Item 1", "Item 2", "Item 3", "Item 4", "Item 5")
+
+        val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, items)
+        listView.adapter = adapter
+
+        listView.onItemClickListener = AdapterView.OnItemClickListener { parent, view, position, id ->
+            val selectedItem = parent.getItemAtPosition(position) as String
+            navigateToDetail(selectedItem)
+        }
+    }
+
+    private fun navigateToDetail(item: String) {
+        val intent = Intent(this, DetailActivity::class.java)
+        intent.putExtra("item", item)
+        startActivity(intent)
     }
 }
